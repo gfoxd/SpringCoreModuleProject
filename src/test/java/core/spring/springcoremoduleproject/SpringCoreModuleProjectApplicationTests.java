790bcfdbcfe187152f23433c126bd14dfc9b762b
Alexander gfoxd <sashanara700@gmail.com>
2025-05-28 15:11:19 +0800

    first commit

---
+++ src/test/java/core/spring/springcoremoduleproject/SpringCoreModuleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package core.spring.springcoremoduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreModuleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
